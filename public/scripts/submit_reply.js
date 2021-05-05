7560577a2023a4e7f02b027d1797e93deffaba57
James Brown <brownj95@gmail.com>
2021-05-05 20:15:26 +0000

    Comments

--- public/scripts/submit_reply.js
+++ public/scripts/submit_reply.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   console.log('loaded');
 
-  $('.send-reply').hide();
+  $('.send-reply').hide(); //=> Hide buttons on page load
 
-  $('.reply-text').on('input', function() {
+  $('.reply-text').on('input', function() { //=> Show buttons if textbox is not empty
     if ($(this).val().length === 0) {
       $(this).siblings('.send-reply').hide();
     } else {
@@ -11,7 +11,7 @@ $(document).ready(function() {
     }
   });
 
-  $('.reply').on('submit', function(event) {
+  $('.reply').on('submit', function(event) { //=> Handle submit reply; send message keys to server side along with message text
     event.preventDefault();
     const fromUser = $('.current-user').text();
     const toUser = $(this).parent().find('.from-user-id').text();
